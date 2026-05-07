ba51126b66dc51506954608d21c723ac1b1a8c96
Aston Arnott (1000122523) <ARNOAO1@student.op.ac.nz>
2026-05-07 14:55:32 +1200

    added course and about sections filled with info

---
+++ backend/controllers/course.js
@@ -0,0 +1,8 @@
+export const getCourse = (req, res) => {
+  res.json({
+    course1: "Introductory App Development",
+    course2: "Introductory Game Development",
+    course3: "Quality Assurance Testing",
+    course4: "Studio 4"
+  });
+};
